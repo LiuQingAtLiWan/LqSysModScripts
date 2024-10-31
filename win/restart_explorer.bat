e0d40ad7f3669f1914181ad97a24d8e6a499ccfb
liuqingatliwan <mstlqbk@gmail.com>
2024-10-31 09:26:57 +0800

    add restart_explorer.bat

---
+++ win/restart_explorer.bat
@@ -0,0 +1,18 @@
+@echo off
+REM ------------------------------------------------
+REM SysModScripts - Windows System Modifications
+REM Created by: LIU Qing
+REM Email: mst.lq.bk+SysMod@gmail.com
+REM Purpose: Restart the explorer process (which is usually used after modifying the registry)
+REM Copyright 2024
+REM ------------------------------------------------
+
+echo Restarting Explorer process...
+
+taskkill /f /im explorer.exe
+
+timeout /t 4  >nul 2>nul
+
+start explorer.exe
+
+echo Explorer restarted successfully.
