b6f7b1057121e43ff72c244688dfd37535041a9c
liuqingatliwan <mstlqbk@gmail.com>
2025-05-12 22:40:26 +0800

    modified date

--- win/restart_explorer.bat
+++ win/restart_explorer.bat
@@ -4,7 +4,7 @@ REM SysModScripts - Windows System Modifications
 REM Created by: LIU Qing
 REM Email: mst.lq.bk+SysMod@gmail.com
 REM Purpose: Restart the explorer process (which is usually used after modifying the registry)
-REM Copyright 2024
+REM Copyright 2025
 REM ------------------------------------------------
 
 echo Restarting Explorer process...
